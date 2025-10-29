b9a90acaebb0bad6bd7bd0e894eceac251cfacfa
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-10-29 06:19:20 +0530

    Project setup & main entry
    Add Main class and DataSorterApp skeleton

--- Main.java
+++ Main.java
@@ -1,1 +1,29 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSorterApp app = new DataSorterApp();
+        app.run();
+    }
+}
+
+class DataSorterApp {
+    public void run() {
+        Utils.ScannerWrapper scanner = new Utils.ScannerWrapper();
+        int[] data = new int[0];
+        int choice;
+
+        do {
+            Utils.printMenu();
+            choice = Utils.getIntInput(scanner, "Enter your choice: ");
+
+            switch (choice) {
+                // to be added in next commits
+            }
+
+        } while (choice != 7);
+
+        scanner.close();
+    }
+}
 
