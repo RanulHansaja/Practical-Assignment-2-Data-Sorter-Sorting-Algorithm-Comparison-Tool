48027ec00d762a4b223e95e58a1ad928a5c24c1c
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-10-29 19:08:51 +0530

    Create Utils class and ScannerWrapper
    Add Utils class with ScannerWrapper for input handling.

--- Utils.java
+++ Utils.java
@@ -1,1 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
 
+public class Utils {
+
+    // Simple wrapper for scanner so we can close it once in main
+    public static class ScannerWrapper {
+        private Scanner scanner = new Scanner(System.in);
+        public String nextLine() { return scanner.nextLine(); }
+        public void close() { scanner.close(); }
+    }
