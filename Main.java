b0512c29050b335ad798e38fa53fa5fd7cebd7b0
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-10-29 07:43:59 +0530

    : Add menu loop and switch structure
    Add menu loop and switch structure.

--- Main.java
+++ Main.java
@@ -12,3 +12,14 @@ class DataSorterApp {
         Utils.ScannerWrapper scanner = new Utils.ScannerWrapper();
         int[] data = new int[0];
         int choice;
+        do {
+            Utils.printMenu();
+            choice = Utils.getIntInput(scanner, "Enter your choice: ");
+            switch (choice) {
+                case 1 -> {
+                    data = Utils.getManualInput(scanner);
+                }
+                case 2 -> {
+                    data = Utils.generateRandomList(scanner);
+                    System.out.println("Generated data: " + Arrays.toString(data));
+                }
