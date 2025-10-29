6975ae2d94b32c4aeed531514620f4581984dc78
Ranul Hansaja <ranulh6@gmail.com>
2025-10-29 09:40:55 +0530

    Add partition method to handle element swapping and pivot positioning

--- QuickSort.java
+++ QuickSort.java
@@ -20,3 +20,22 @@ public class QuickSort {
             quickSort(arr, pi + 1, high);
         }
     }
+
+      private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            stepsCounter++;
+            if (arr[j] < pivot) {
+                i++;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        int tmp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = tmp;
+        return i + 1;
+    }
+}
