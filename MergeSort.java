6a08d435176544e7a44685be91fe3558c9fdddbc
sankalpi-2001 <sathyasankalpi2001@gmail.com>
2025-10-30 09:52:14 -0700

    Class setup and method declaration

--- MergeSort.java
+++ MergeSort.java
@@ -1,1 +1,12 @@
+public class MergeSort {
+    private static long stepsCounter;
 
+    public static Utils.SortResult sort(int[] input) {
+        int[] arr = java.util.Arrays.copyOf(input, input.length);
+        stepsCounter = 0;
+        long start = System.nanoTime();
+        mergeSort(arr, 0, arr.length - 1);
+        long end = System.nanoTime();
+        double timeMs = (end - start) / 1e6;
+        return new Utils.SortResult(arr, timeMs, stepsCounter);
+    }
