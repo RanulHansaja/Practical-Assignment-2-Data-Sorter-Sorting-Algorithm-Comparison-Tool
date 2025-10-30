8d422f1e3e38160647f760d5d4ffbefeffc8c90e
sankalpi-2001 <sathyasankalpi2001@gmail.com>
2025-10-30 09:53:52 -0700

    Recursive mergeSort logic

--- MergeSort.java
+++ MergeSort.java
@@ -1,3 +1,5 @@
+import org.jcp.xml.dsig.internal.dom.Utils;
+
 public class MergeSort {
     private static long stepsCounter;
 
@@ -9,4 +11,11 @@ public class MergeSort {
         long end = System.nanoTime();
         double timeMs = (end - start) / 1e6;
         return new Utils.SortResult(arr, timeMs, stepsCounter);
+    }
+    rivate static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
