57076416c140738ee420369f425cc05d06ef69c6
Sithija194 <sithijant231@gmail.com>
2025-10-31 10:32:14 +0530

    Makes a copy of the input array, initializes step counter and timer, and gets array length.

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,1 +1,6 @@
-
+public class BubbleSort {
+    public static Utils.SortResult sort(int[] input) {
+        int[] arr = java.util.Arrays.copyOf(input, input.length);
+        long steps = 0;
+        long start = System.nanoTime();
+        int n = arr.length;
