a1306886131564b2b2568f9ed61eeb4d7929d606
Ranul Hansaja <ranulh6@gmail.com>
2025-10-31 13:36:06 +0530

    Add partition method

--- QuickSort.java
+++ QuickSort.java
@@ -18,3 +18,22 @@ public static Utils.SortResult sort(int[] input) {
         }
     }
 
+        private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            stepsCounter++;
+            if (arr[j] < pivot) {
+                i++;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        int tmp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = tmp;
+        return i + 1;
+    }
+}
+
