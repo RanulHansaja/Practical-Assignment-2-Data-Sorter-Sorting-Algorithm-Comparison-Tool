b0ff410b3bd4dbdf4aec99265806f138f8fecacd
Sithija194 <sithijant231@gmail.com>
2025-10-31 10:35:11 +0530

    Repeatedly compares and swaps adjacent elements to sort the array in ascending order.

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,3 +4,13 @@ public class BubbleSort {
         long steps = 0;
         long start = System.nanoTime();
         int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                steps++;
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
